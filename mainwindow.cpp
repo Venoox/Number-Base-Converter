1b74e31e9bc97e38c4d30d5d58a353fd630582b9
venoox <venooxpersonal@gmail.com>
2017-12-05 17:32:41 +0100

    Now works: 2->8, 8->2, 10->2, 10->8

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,40 +7,60 @@ MainWindow::MainWindow(QWidget *parent) :
 {   this->setFixedSize(QSize(396,336));
     ui->setupUi(this);
 }
-
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 // Check how long is a number
-int howlong (unsigned long long x) {
+unsigned long long howlong (unsigned long long x) {
     int lengthCount = 0;
     for(; x != 0; x /= 10, lengthCount++);
     return lengthCount;
 }
+unsigned long long value;
+unsigned long long DecToBin(unsigned long long n)
+{
+    std::string r;
+    while(n!=0) {
+        r=(n%2==0 ?"0":"1")+r;
+        n/=2;
+    }
+    value = atoi(r.c_str());
+    return value;
+}
+unsigned long long DecToOct(unsigned long long n)
+{
+    std::string r;
+    while(n!=0) {
+        r=(n%8==0 ? "0": n%8==1 ? "1": n%8==2 ? "2":n%8==3 ? "3":n%8==3 ? "3":n%8==5 ? "5":n%8==6 ? "6":n%8==7 ? "7": "error")+r;
+        n/=8;
+    }
+    value = atoi(r.c_str());
+    return value;
+}
 // 2 -> 10
-int bintodec(unsigned long long whole) {
+unsigned long long BinToDec(unsigned long long n) {
     unsigned long long dec = 0, base = 1;
     short rem;
-    while (whole > 0)
+    while (n > 0)
     {
-        rem = whole % 10; //
+        rem = n % 10; //
         dec += + rem * base;
         base *= 2;
-        whole /= 10;
+        n /= 10;
     }
         return dec;
 }
 // 8 -> 10
-int octtodec(unsigned long long whole) {
+unsigned long long OctToDec(unsigned long long n) {
     unsigned long long dec = 0, base = 1;
     short rem;
-    while (whole > 0)
+    while (n > 0)
     {
-        rem = whole % 10;
+        rem = n % 10;
         dec += + rem * base;
         base *= 8;
-        whole /= 10;
+        n /= 10;
     }
         return dec;
 }
@@ -60,29 +80,33 @@ void MainWindow::on_pushButton_clicked()
        ui->answerbox->setText(input);
     }
     else if (basefrom==0&&baseto==1) { // 2 -> 8
-
+        ui->answerbox->setText(QString::number(DecToOct(BinToDec(number))));
     }
     else if (basefrom==0&&baseto==2) { // 2 -> 10
-        ui->answerbox->setText(QString::number(bintodec(number)));
-
+        //if (input.contains("0")&&input.contains("1")) {
+            ui->answerbox->setText(QString::number(BinToDec(number)));
+        //}
+        //else {
+        //    ui->answerbox->setText("Error");
+        //}
     }
     else if (basefrom==0&&baseto==3) { // 2 -> 16
 
     }
     else if (basefrom==1&&baseto==0) { // 8 -> 2
-
+        ui->answerbox->setText(QString::number(DecToBin(OctToDec(number))));
     }
     else if (basefrom==1&&baseto==2) { // 8 -> 10
-        ui->answerbox->setText(QString::number(octtodec(number)));
+        ui->answerbox->setText(QString::number(OctToDec(number)));
     }
     else if (basefrom==1&&baseto==3) { // 8 -> 16
 
     }
     else if (basefrom==2&&baseto==0) { // 10 -> 2
-
+        ui->answerbox->setText(QString::number(DecToBin(number)));
     }
     else if (basefrom==2&&baseto==1) { // 10 -> 8
-
+        ui->answerbox->setText(QString::number(DecToOct(number)));
     }
     else if (basefrom==2&&baseto==3) { // 10 -> 16
 
